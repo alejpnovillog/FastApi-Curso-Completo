491b5f587d6c7cdb91a58f79ef2238fa7a0ef40c
alejpnovillog <[alejpnovillog@gmail.com]>
2023-10-02 16:43:25 -0300

    Retornamos un xml

---
+++ Retorna_Respuesta_XML/Ejemplo_00.py
@@ -0,0 +1,38 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# controlamos las librerias que importamos
+try:
+
+    #  Uvicorn es un servidor web ASGI (interfaz de puerta de enlace de servidor asíncrono)
+    import uvicorn
+
+    # utilizamos la clase FastApi
+    from fastapi import FastAPI, Response
+
+except Exception as e:
+    print(f'Falta algun modulo en Ejemplo_00 --> {e}')
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# creamos una instancia de la clase  FastApi
+app = FastAPI()
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# En esta operacion de ruta
+@app.get("/legacy/")
+def get_legacy_data():
+    data = """<?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type="application/xml")
+
+
+# Llamamos al servidor para que inicie
+if __name__ ==  "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
