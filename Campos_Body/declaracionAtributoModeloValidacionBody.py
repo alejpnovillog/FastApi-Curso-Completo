88345d89227e27b964aed51add980cb827ac4289
alejpnovillog <[alejpnovillog@gmail.com]>
2023-10-09 15:17:01 -0300

    Documentacion de Campos de Body

---
+++ Campos_Body/declaracionAtributoModeloValidacionBody.py
@@ -0,0 +1,56 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# controlamos las librerias que importamos
+try:
+
+    #  Uvicorn es un servidor web ASGI (interfaz de puerta de enlace de servidor asíncrono)
+    import uvicorn
+
+    # utilizamos la clase FastApi
+    from fastapi import Body, FastAPI
+
+    #
+    from pydantic import BaseModel, Field
+
+    from typing import Annotated
+
+except Exception as e:
+    print(f'Falta algun modulo en Ejemplo_00 --> {e}')
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# creamos una instancia de la clase  FastApi
+app = FastAPI()
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# definimos un modelo de validacion de datos en el body
+# los atributos no requeridos son
+#   description, tax
+# los campos description y price son campos con metadatos para su validacion
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(default=None, title="The description of the item", max_length=300)
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float | None = None
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# En esta operacion de ruta
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    """
+
+    item_id:        Es un parametro requerido de tipo integer
+    item:           Es un parametro de tipo objeto Item  embebido  en el body
+
+    """
+
+    # asignamos un diccionario
+    results = {"item_id": item_id, "item": item}
+
+
+    return results
+
+
+# Llamamos al servidor para que inicie
+if __name__ ==  "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
