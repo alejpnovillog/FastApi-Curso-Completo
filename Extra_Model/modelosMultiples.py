f4de7c910065f8ceee5804c8b63b3ab5a0c70263
alejpnovillog <[alejpnovillog@gmail.com]>
2023-10-19 12:06:09 -0300

    Documentacion Extra Model

--- Extra_Model/modelosMultiples.py
+++ Extra_Model/modelosMultiples.py
@@ -32,17 +32,36 @@ class UserBase(BaseModel):
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # DEFINIMOS UN MODELO DE VALIDACION DE DATOS EN EL BODY
 class UserIn(UserBase):
+    """
+
+    Args:
+        UserBase (_type_):  La clase hereda del modelo UserBase
+    """
+    
     password: str
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # DEFINIMOS UN MODELO DE VALIDACION DE DATOS EN EL BODY
 class UserOut(UserBase):
+    """
+
+    Args:
+        UserBase (_type_):  La clase hereda del modelo UserBase
+                            Se la utiliza para responder con el contenido de la
+                            clase UserBase
+    """
+    
     pass
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # DEFINIMOS UN MODELO DE VALIDACION DE DATOS EN EL BODY
 class UserInDB(UserBase):
+    """
+
+    Args:
+        UserBase (_type_):  La clase hereda del modelo UserBase
+    """
     hashed_password: str
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -58,11 +77,28 @@ def fake_password_hasher(raw_password: str):
     """
     return "supersecret" + raw_password
 
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # DEFINIMOS UNA FUNCION QUE RETORNA EL HASH DE LA PASSWORD ENVIADA EN UN MODELO DE VALIDACION
 def fake_save_user(user_in: UserIn):
+    """
+
+    Args:
+        user_in (UserIn):   El parametro es de tipo del modelo de validacion UserIn
+
+    Returns:
+        _type_:             La instancia user_in_db
+    """
+    
+    # OBTENEMOS EL HASH DEL STRING QUE LE PASAMOS COMO PARAMETRO
     hashed_password = fake_password_hasher(user_in.password)
+    
+    # ASIGNAMOS UNA INSTANCIA DE LA CLASE UserInDb PASANDOLE COMO  PARAMETRO
+    # EL MODELO UserIn COMO DICCIONARIO Y PASANDOLE EL VALOR DEL ATRIBUTO
+    # DE LA CLASE UserInDb
     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    
+    
     print("User saved! ..not really")
     return user_in_db
 
@@ -71,7 +107,19 @@ def fake_save_user(user_in: UserIn):
 # EN ESTA OPERACION DE RUTA
 @app.post("/user/", response_model=UserOut)
 async def create_user(user_in: UserIn):
+    """
+
+    Args:
+        user_in (UserIn):          El parametro es de tipo del modelo de validacion UserIn
+
+    Returns:
+        _type_: _description_
+        
+    """
+    
+    # RETORNAMOS LA INSTANCIA DE LA CLASE UserInDb
     user_saved = fake_save_user(user_in)
+    
     return user_saved
 
 
